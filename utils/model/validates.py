2088bb0f8f5d2c2e867d91481c6d3cd861b03fc6
LinChi <aa35205905@gmail.com>
2021-08-12 05:46:29 +0800

    feat: 新增圖片欄位檔案大小驗證

---
+++ utils/model/validates.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+
+def validate_image(image):
+    file_size = image.file.size
+    limit_mb = 2
+
+    if file_size > limit_mb * 1024 * 1024:
+       raise ValidationError("大小不超過：{} MB".format(limit_mb))
