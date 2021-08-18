ad094733713e3ad266ca5896ae7d9471f61e3365
LinChi <aa35205905@gmail.com>
2021-08-18 16:42:17 +0800

    feat: 新增PDF檔案大小驗證

--- utils/model/validates.py
+++ utils/model/validates.py
@@ -6,3 +6,10 @@ def validate_image(image):
 
     if file_size > limit_mb * 1024 * 1024:
        raise ValidationError("大小不超過：{} MB".format(limit_mb))
+
+def validate_pdf(pdf):
+    file_size = pdf.file.size
+    limit_mb = 10
+
+    if file_size > limit_mb * 1024 * 1024:
+       raise ValidationError("大小不超過：{} MB".format(limit_mb))
